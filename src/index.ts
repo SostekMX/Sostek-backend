1aa2170927c7851ac24fa488354e006b980c5b7e
Fernanda Sánchez <mafsr1999@gmail.com>
2022-11-11 17:25:49 -0600

    Added line to prevent CORS error

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,12 @@ const PORT = process.env.PORT || 8080;
 const JWT_CODE = process.env.JWT_CODE || "tcjAU[xqQ]px9x&X(()KQhpAg=@P=cbJRJ7DZ$,:ZhW8gD#N@)r;S6TH$5u==nDaMjWd%:Jn.rS,QqkCyV*}v,UArB!_V7.+-mKZehZwCMbY/Dj69X.YKL$#byG7b.4%_tJjjG6=AeTLVinW2iGDuF*jeRX;a(S,/6]#*?3d:xT-/E2L6S$=j_,[6;(uy7cJz+]_9K5RTJd6re9e[@k@BxK,W#=ZRbT)/A2J,vfee2a%+Sc4!BW73Wdyn/r@na3?:FiL?D,nN%9Q7;H_H3!Fg.(i;bTJywEK-GXikn#(+U*}";
 
 const app = express()
+app .use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json())
 
 
 app.post("/user/signup", (req: Request, res: Response) => {
+
   if(!req.body.email || !req.body.password || !req.body.name || !req.body.surname) {
     res.json({ success: false, error: "Required params missing" });
     return;
